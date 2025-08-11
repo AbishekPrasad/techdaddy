843afa93c250a6254e79b5e0fef3bc07a6436d05
Abishek Prasad Subramanian <110110648+AbishekPrasad@users.noreply.github.com>
2025-08-11 19:34:45 +0530

    Update Pricing.jsx

--- techdaddy_portfolio/src/components/Pricing.jsx
+++ techdaddy_portfolio/src/components/Pricing.jsx
@@ -41,7 +41,7 @@ function PricingPlan({ title, price, features }) {
       className="p-8 rounded-lg bg-accent/20 border border-primary shadow-xl hover:shadow-neon/40 transition-shadow duration-300 text-center flex flex-col"
     >
       <h3 className="text-2xl font-semibold text-neon font-mono mb-4">{title}</h3>
-      <div className="mt-4 text-4xl font-bold text-light mb-6">{price}</div>
+      <div className="mt-4 text-4xl font-bold text-light mb-6" style={{fontFamily:"monospace"}}>{price}</div>
       <ul className="mt-4 text-light/80 flex-grow space-y-2">
         {features.map((f, index) => (
           <li key={index}>{f}</li>
